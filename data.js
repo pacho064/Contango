3af4ed5aac8c60d9447cbf346b8da9ed30541ae2
Contango-Robot <robot@contango.energy>
2026-03-22 09:04:19 +0000

    Robot: Updated Live Prices

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 const CONTANGO_DATA = {
-  lastUpdated: "2026-03-22 07:52 UTC",
+  lastUpdated: "2026-03-22 09:04 UTC",
   prices: [
-    {sym: 'BRENT', name: 'ICE Brent', val: 82.59, chg: 1.2, origin: 'Global'},
-    {sym: 'ARAB L', name: 'Arab Light', val: 84.05000000000001, chg: 0.8, origin: 'Saudi Arabia'},
-    {sym: 'MURBAN', name: 'Murban', val: 83.41000000000001, chg: 1.1, origin: 'UAE'},
-    {sym: 'OMAN', name: 'Oman/Dubai', val: 81.09, chg: -0.4, origin: 'DME'}
+    {sym: 'BRENT', name: 'ICE Brent', val: 82.01, chg: 1.2, origin: 'Global'},
+    {sym: 'ARAB L', name: 'Arab Light', val: 83.96000000000001, chg: 0.8, origin: 'Saudi Arabia'},
+    {sym: 'MURBAN', name: 'Murban', val: 83.89, chg: 1.1, origin: 'UAE'},
+    {sym: 'OMAN', name: 'Oman/Dubai', val: 81.24000000000001, chg: -0.4, origin: 'DME'}
   ],
   intel: [
     {tag: 'LOGISTICS', hl: 'Suez Transit Risk Elevated', txt: 'Shipping volumes down 15% WoW.', time: 'Live'}
